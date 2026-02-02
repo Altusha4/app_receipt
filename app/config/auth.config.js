984b7c74ee4f7493968e1e6662c6ba61dae4b996
Алтынай Ертай <altynayyertay@MacBook-Air-Altynaj.local>
2026-02-02 13:51:24 +0500

    feat: complete project backend setup with working auth and DB connection

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "receipt-secret-key"
+};
--- server.js
+++ server.js
@@ -5,13 +5,23 @@ const db = require("./app/models");
 
 const app = express();
 
-app.use(cors());
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+const Role = db.role;
 
 db.mongoose
     .connect(db.url)
     .then(() => {
         console.log("Successfully connected to MongoDB Atlas!");
+        initial();
     })
     .catch(err => {
         console.error("Connection error", err);
@@ -22,7 +32,38 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to App Receipt application." });
 });
 
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
+
+function initial() {
+    Role.estimatedDocumentCount()
+        .then(count => {
+            if (count === 0) {
+                new Role({ name: "user" })
+                    .save()
+                    .then(() => {
+                        console.log("added 'user' to roles collection");
+                    })
+                    .catch(err => {
+                        console.log("error adding user role", err);
+                    });
+
+                new Role({ name: "admin" })
+                    .save()
+                    .then(() => {
+                        console.log("added 'admin' to roles collection");
+                    })
+                    .catch(err => {
+                        console.log("error adding admin role", err);
+                    });
+            }
+        })
+        .catch(err => {
+            console.log("error on estimatedDocumentCount", err);
+        });
+}
