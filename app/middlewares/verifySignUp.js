bc1a94ccf194fff248b213d1d7651a1627715234
Алтынай Ертай <altynayyertay@MacBook-Air-Altynaj.local>
2026-02-02 15:47:08 +0500

    feat: successfully implemented user registration with Promises and MongoDB

--- app/middlewares/verifySignUp.js
+++ app/middlewares/verifySignUp.js
@@ -3,38 +3,33 @@ const ROLES = db.ROLES;
 const User = db.user;
 
 const checkDuplicateUsernameOrEmail = (req, res, next) => {
-    User.findOne({ username: req.body.username }).exec((err, user) => {
-        if (err) {
-            res.status(500).send({ message: err });
-            return;
-        }
-        if (user) {
-            res.status(400).send({ message: "Failed! Username is already in use!" });
-            return;
-        }
-
-        User.findOne({ email: req.body.email }).exec((err, user) => {
-            if (err) {
-                res.status(500).send({ message: err });
-                return;
-            }
+    User.findOne({ username: req.body.username })
+        .then(user => {
             if (user) {
-                res.status(400).send({ message: "Failed! Email is already in use!" });
-                return;
+                return res.status(400).send({ message: "Failed! Username is already in use!" });
             }
+
+            return User.findOne({ email: req.body.email });
+        })
+        .then(user => {
+            if (user) {
+                return res.status(400).send({ message: "Failed! Email is already in use!" });
+            }
+
             next();
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message || "Some error occurred while checking user." });
         });
-    });
 };
 
 const checkRolesExisted = (req, res, next) => {
     if (req.body.roles) {
         for (let i = 0; i < req.body.roles.length; i++) {
             if (!ROLES.includes(req.body.roles[i])) {
-                res.status(400).send({
+                return res.status(400).send({
                     message: `Failed! Role ${req.body.roles[i]} does not exist!`
                 });
-                return;
             }
         }
     }
